a606a0d228bc22cd41d427c237268614c3ff2453
nicebeam <nicebeam@users.noreply.github.com>
2023-01-13 12:05:09 +0000

    Adding tags and meta data

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import react from "react"
+
+function footer() {
+  return (
+  <div>Navigation map</div>
+  <div>Contact</div>
+  <div>Social Media</div>
+  );
+}
+
+export default footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import react from "react"
+import logo from './logo.svg';
+
+function Nav() {
+    return (
+    <img src="./logo.svg"/>
+        <ul>
+        <li><a href="">Home</li>
+        <li><a href="">About</li>
+        <li><a href="">Menu</li>
+        <li><a href="">Reservations</li>
+        <li><a href="">Order Online</li>
+        <li><a href="">Login</li>
+    </ul>
+    )
+}
+
+export default Nav;
